8aec1623d1410b5cf036c464d5afc519b86bcde7
Victor Luz <victorlanceluz@outlook.com>
2022-03-21 19:48:39 -0700

    New updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # friendly-octo-carnival
 A project by Justin Naorbe and Victor Luz
+
+Robot car that follows a laser pointer.
---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+
+Main testing file.
+
+*/ 
+
+#include <iostream>
+#include <string>
+
+#define CONSTANT 1
+
+void main(void)
+{
+    if CONSTANT == 1)
+}
